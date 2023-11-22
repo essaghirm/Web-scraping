39004657899368d8dd5a0ca977bdf9106b4d7e66
Mouhcine Essaghir <34938901+essaghirm@users.noreply.github.com>
2023-11-22 13:17:00 +0100

    push

--- marjanemall.js
+++ marjanemall.js
@@ -1,7 +1,8 @@
 var items = []
 localStorage.setItem("items", JSON.stringify(items))
+var json;
 
-for(i=1; i<=702; i++){
+for(i=1; i<=10; i++){
     jQuery.ajax({
         url: `https://www.marjanemall.ma/all-products?m_shipping_location=LOCAL&p=${i}&product_list_limit=80`,
         type: 'GET',
@@ -19,16 +20,44 @@ for(i=1; i<=702; i++){
             var p = jQuery(page).find('.products.wrapper~.toolbar-products .item.pageNumber.current span:last-child').text()
             console.log(`Page ${p}:${k} - ` +  jQuery(this).find('.product-item-link').text())
 
-            items.push({
-                page: `${p}:${k}`,
+            var onclick = jQuery(this).find(".product.product-item-photo").attr("onclick");
+            onclick = onclick.split(";")[1]
+            // console.log(onclick)
+            onclick = onclick.replace(" window.dataLayer.push(", "")
+            onclick = onclick.replace("/.$/", "")
+            // console.log(onclick)
+            onclick = JSON.parse(onclick)
+            // console.log('ecommerce', onclick.ecommerce)
+            // json = JSON.parse(onclick)
+
+            var item = {
+                page: p,
+                position: k,
                 title: jQuery(this).find('.product-item-link').text(),
-                soldby: jQuery(this).find('.page-products .sell-by strong').text(),
+                soldby: jQuery(this).find('.sell-by strong').text(),
                 price: jQuery(this).find('.price').text(),
-            });
+                rating: jQuery(this).find('.rating-summary .rating-result').attr("title"),
+                reviews: jQuery(this).find('.reviews-actions a').text(),
+                item_id: onclick.ecommerce.items[0].item_id,
+                item_brand: onclick.ecommerce.items[0].item_brand,
+                affiliation: onclick.ecommerce.items[0].affiliation,
+                item_category: onclick.ecommerce.items[0].item_category,
+                item_category2: onclick.ecommerce.items[0].item_category2,
+                item_category3: onclick.ecommerce.items[0].item_category3,
+                item_category4: onclick.ecommerce.items[0].item_category4,
+                item_category5: onclick.ecommerce.items[0].item_category5,
+                url: jQuery(this).find('.product-item-link').attr('href'),
+            }
+
+            items.push(item);
+            console.log(item)
+
+            if(k == 5) {
+                // return false; // breaks
+            }
             // console.log(items)
         })
 
         localStorage.setItem("items", JSON.stringify(items))
     });
 }
-
