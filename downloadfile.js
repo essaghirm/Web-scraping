e43bb04cd45630451872995c0c4b4f803a821754
Mouhcine Essaghir <34938901+essaghirm@users.noreply.github.com>
2023-12-11 17:25:45 +0100

    Delete downloadfile.js

--- downloadfile.js
+++
@@ -1,5 +0,0 @@
-var bb = new Blob([localStorage.getItem("items")], { type: 'application/json' });
-var a = document.createElement('a');
-a.download = 'download.json';
-a.href = window.URL.createObjectURL(bb);
-a.click();
