3d635baeebb3feccbfc6c6649fec8241cc4cbd2f
Mouhcine Essaghir <34938901+essaghirm@users.noreply.github.com>
2023-12-11 17:25:42 +0100

    Create doanloadJson.js

---
+++ doanloadJson.js
@@ -0,0 +1,7 @@
+function doanloadJson() {
+    var bb = new Blob([localStorage.getItem("items")], { type: 'application/json' });
+    var a = document.createElement('a');
+    a.download = 'download.json';
+    a.href = window.URL.createObjectURL(bb);
+    a.click();
+}
